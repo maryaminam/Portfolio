271cb46090c4d2f87ce143f2d23db35bb55f86ba
Maryam <maryaminam4113@gmail.com>
2024-08-02 19:01:47 +0500

    second commi

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -4,6 +4,7 @@ import atm from '../../assets/ATM.png';
 import hms from '../../assets/HMS.png';
 import styles from './ProjectsStyles.module.css';
 import ProjectCard from '../../common/ProjectCard';
+import portfolio from '../../assets/portfolio.png';
 
 function Projects() {
   return (
@@ -13,6 +14,7 @@ function Projects() {
           <ProjectCard src={CPU} link="https://github.com/maryaminam/CPU-Scheduler" h3="CPU Scheduler"/>
           <ProjectCard src={hms} link="https://github.com/maryaminam/Hospital-Management-System-in-JAVA" h3="Hospital Management System"/>
           <ProjectCard src={atm} link="https://github.com/maryaminam/ATM-" h3="ATM System"/>
+          <ProjectCard src={portfolio} link="https://github.com/maryaminam/Portfolio/tree/main" h3="Portfoilo"/>
           
         </div>
     </section>    
