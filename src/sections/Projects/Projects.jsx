76f9a643de9a40ce7b1f9f882fdc2cd5a5a8223b
Maryam <maryaminam4113@gmail.com>
2024-09-02 15:55:56 +0500

    commit

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -17,7 +17,7 @@ function Projects() {
         <div className={styles.projectsContainer}>
           <ProjectCard src={CPU} link="https://github.com/maryaminam/CPU-Scheduler" h3="CPU Scheduler"/>
           <ProjectCard src={hms} link="https://github.com/maryaminam/Hospital-Management-System-in-JAVA" h3="Hospital Management System"/>
-          <ProjectCard src={atm} link="https://github.com/maryaminam/ATM-" h3="ATM System"/>
+          <ProjectCard src={atm} link="https://github.com/maryaminam/ATM" h3="ATM System"/>
           <ProjectCard src={portfolio} link="https://github.com/maryaminam/Portfolio/tree/main" h3="Portfolio"/>
           <ProjectCard src={ims} link="https://github.com/maryaminam/Inventory-Management-System" h3="Inventory Management System"/>
           <ProjectCard src={blog} link="https://github.com/maryaminam/BLOG" h3="Blog Website"/>
